0923e0fc33b07cf5c9dad738525b41b30a50335d
Richard de Rozario <richard.derozario@gmail.com>
2025-10-20 15:40:50 +1100

    changed version to accommodate SWI-PL archive

--- src/pack.pl
+++ src/pack.pl
@@ -1,6 +1,6 @@
 name(pro2sql).
 title('Lightweight translater of Prolog to SQL SELECT statements').
-version('0.1.00').
+version('0.1.0').
 author('Richard de Rozario','richard.derozario@gmail.com').
 home('https://github.com/RdR1024/pro2sql').
-download('https://github.com/RdR1024/pro2sql/raw/refs/heads/main/rel/pro2sql-0.1.00.zip').
+download('https://github.com/RdR1024/pro2sql/raw/refs/heads/main/rel/pro2sql-0.1.0.zip').
